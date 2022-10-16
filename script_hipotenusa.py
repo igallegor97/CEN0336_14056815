00aca72d4683faf50fb5a40b602fa5389e2d2a56
Isa Gallego <88630062+igallegor97@users.noreply.github.com>
2022-10-16 20:34:55 -0300

    Update script_hipotenusa.py

--- script_hipotenusa.py
+++ script_hipotenusa.py
@@ -1,40 +1,37 @@
+#Importar librerías
+#Libreria sys: Sirve para manejar los argumentos de entrada
 import sys
+#Libreria math: Sirve para realizar operaciones matematicas
 import math
 
-#print("\nArguments passed:", end = " ")
-#for i in range(1, n):
-#    print(sys.argv[i], end = " ")
+# Comprobar que a y b sean positivos
 
-#print(isinstance(float(sys.argv[1]), int))
-#REVISAR
-if type(int(sys.argv[1])) == int:
-    print("Funcionaaaaaaa!!!")
-else:
-    print("No funcionaaaaaa")
+if int(sys.argv[1]) < 0:
+    sys.exit("a es negativo")
 
-
-#def is_integer_num(n)
-
-if int(sys.argv[1]) < 0:       
-    # exits the program 
-    sys.exit("a es negativo")     
- 
 if int(sys.argv[2]) < 0:
-    # exits the program 
-    sys.exit("b es negativo")   
+    sys.exit("b es negativo")
+
+# Comprobar que a y b sean enteros
+if bool(sys.argv[1].isdigit()) is False:
+        print("a no es un entero")
+        sys.exit()
+
+if bool(sys.argv[2].isdigit()) is False:
+        print("b no es un entero")
+        sys.exit()
+
+# Comprobar que a y b sean menores que 500
 
 if int(sys.argv[1]) > 500:
-    # exits the program 
     sys.exit("a es mayor de 500")
 
 if int(sys.argv[2]) > 500:
-    # exits the program 
     sys.exit("b es mayor de 500")
 
-
-
-
+# Calcular el cuadrado de la hipotenusa
 
 H=int(sys.argv[1])**2+int(sys.argv[2])**2
 
 print("\n\nO quadrado da hipotenusa para o triangulo retângulo com lados a=" + str(sys.argv[1]) + " e b=" + str(sys.argv[2]) + " é:", H)
+
